9391a91dc4f4a1ec7b463b988be09f90ec8d376b
Gabriel Antoniak <gjantoniak@gmail.com>
2018-07-30 21:31:24 -0400

    Adds motor control to the framework

--- FlyAutomatedFramework/FlyAutomatedFramework.ino
+++ FlyAutomatedFramework/FlyAutomatedFramework.ino
@@ -1,17 +1,23 @@
 /*
  Name:		FlyAutomatedFramework.ino
  Created:	7/30/2018 2:34:43 PM
- Author:	Gabriel J. Antoniak
+ Author:	Gabriel Antoniak
 */
 
-const int ms1Pin = 2;
-const int ms2Pin = 3;
+//MS pins on motor driver stepper
+const int ms1Pin = 2;	//doubles stepping (1 -> 2, or 4 -> 8)
+const int ms2Pin = 3;	//quadruples stepping (1 -> 4)
+const int ms3Pin = 11;	//doubles stepping (only from 8 -> 16)
 
-const int stepPin = 4;
-const int dirPin = 5;
+const int readMS1 = 8;
+const int readMS2 = 9;
+const int readMS3 = 10;
 
-const int relayLogic1 = 6;
-const int relayLogic2 = 7;
+const int stepPin = 4;	//send pulse to move motor
+const int dirPin = 5;	//set to high or low to change direction
+
+const int relayLogic1 = 6;	//activate relay 1
+const int relayLogic2 = 7;	//activate relay 2
 
 const int can_fr_top_IR = A0;
 const int can_fr_bot_IR = A1;
@@ -25,17 +31,55 @@ const int con_bk_bot_IR = A7;
 
 const int align_IR = A8;
 
-const int circDegree = 360;
-const int numFlyTubes = 6;
+int motor1BaseSteps = 400;
+int numFlyTubes = 6;
 
-// the setup function runs once when you press reset or power the board
+int stepsPerRot;
+int stepsToNextFly;
+int stepErrorIter;
+
+float error = 0;
+
+int toStep;
+
 void setup() {
-
+	stepsPerRot = findStepsPerRot(motor1BaseSteps);
+	stepsToNextFly = findStepsToNextFly(stepsPerRot, numFlyTubes);
+	stepErrorIter = findStepErrorIter(stepsToNextFly, stepsPerRot, numFlyTubes);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	error = stepMotor(toStep, 400, true, error);
+	delay(1000);
+}
+
+float findStepErrorIter(int stepsNext, int stepsRot, int numFlies){
+	int errorPerFullRot = stepsRot - numFlies*stepsNext;
+	float errorIter = ((float) errorPerFullRot) / numFlies;
+	return errorIter;
+}
+
+int findStepsToNextFly(int stepsPerRot, int numFlyTubes){
+	float circRot = 360.0;
+	int stepsToFly = floor((circRot / ((float) numFlyTubes)) * ((float) stepsPerRot));
+	return stepsToFly;
+}
+
+int findStepsPerRot(int baseSteps){
+	if (digitalRead(readMS1) == 1)
+	{
+		baseSteps = baseSteps * 2;
+	}
+
+	if (digitalRead(readMS2) == 1){
+		baseSteps = baseSteps * 2;
+	}
+
+	if (digitalRead(readMS3) == 1){
+		baseSteps = baseSteps * 2;
+	}
+
+	return baseSteps;
 }
 
 void moveMotor(int speedVal){
@@ -54,10 +98,17 @@ void chooseMotorDir(bool dir){
 	}
 }
 
-void stepMotor(int steps, int speedVal, bool dir){
+
+
+float stepMotor(int steps, int speedVal, bool dir, float err){
 	chooseMotorDir(dir);
+	if (err > 1.0){
+		steps = steps + 1;
+		err = err - 1.0;
+	}
 	for (size_t i = 0; i < steps; i++)
 	{
 		moveMotor(speedVal);
 	}
+	return err;
 }
