6deab6b35f458cb4f101b571b953bfbacc226475
Gabriel Antoniak <gja8@duke.edu>
2018-10-31 16:28:06 -0400

    Changes relay test for assymetric on and off time for relays

--- RelayTest/RelayTest.ino
+++ RelayTest/RelayTest.ino
@@ -3,10 +3,13 @@ const int relayPin = 5;
 void setup() {
   // put your setup code here, to run once:
   pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(5000);
   digitalWrite(relayPin, !digitalRead(relayPin));
-  delay(2500);
+  delay(1000);
+  digitalWrite(relayPin, !digitalRead(relayPin));
 }
