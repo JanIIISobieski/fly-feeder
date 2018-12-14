cad26400c03ce009ad9fc32ce78e636d6d4d1da3
Gabriel Antoniak <gja8@duke.edu>
2018-12-14 17:50:28 -0500

    Updates framework for automated alignment

--- FlyAutomatedFramework/FlyAutomatedFramework.ino
+++ FlyAutomatedFramework/FlyAutomatedFramework.ino
@@ -4,144 +4,107 @@
  Author:	Gabriel Antoniak
 */
 
-//MS pins on motor driver stepper
-const int ms1 = 9;	//doubles stepping (1 -> 2, or 4 -> 8)
-const int ms2 = 10;	//quadruples stepping (1 -> 4, 2-> 8)
-const int ms3 = 11;	//doubles stepping (only from 8 -> 16)
+#include <math.h>
 
-const int stepPin = 5;	//send pulse to move motor
-const int dirPin = 4;	//set to high or low to change direction
+const int pcbPotPin = A0;
+const int pcbRawPin = A1;
+const int pcbAlignPin = 3;
 
-const int relayLogic1 = 6;	//activate relay 1
-const int relayLogic2 = 7;	//activate relay 2
+const int stepPin = 12;
+const int dirPin = 13;
 
-const int align_IR = A0;
-const int fly_IR = A1;
+float expectedSteps = 1600.0;
+const int numTubes = 8;
 
-const int align_digital = 2;
-const int fly_digital = 3;
-volatile int spin_motor = 1;
+volatile bool spinState = true;
 
-int motor1BaseSteps = 400;
-int numFlyTubes = 8;
+int flyIndex = 0;
 
-int stepsPerRot;
-int stepsToNextFly;
 
-char aligned[] = "Cartridge aligned";
-char fly_passed[] = "Fly passed";
+enum DeviceCases
+{
+	listen,
+	align,
+	fireFly,
+	verify,
+	suckFly
+};
+
+int switchCase = listen;
 
 void setup() {
-	Serial.begin(9600);
-
-	//pinMode(ms1, OUTPUT);
-	//pinMode(ms2, OUTPUT);
-	//pinMode(ms3, OUTPUT);
-
-	//digitalWrite(ms1, LOW);
-	//digitalWrite(ms2, LOW);
-	//digitalWrite(ms3, LOW);
+	pinMode(pcbPotPin, INPUT);
+	pinMode(pcbRawPin, INPUT);
+	pinMode(pcbAlignPin, INPUT);
 
 	pinMode(stepPin, OUTPUT);
 	pinMode(dirPin, OUTPUT);
 
-	digitalWrite(stepPin, LOW);
+	attachInterrupt(digitalPinToInterrupt(pcbAlignPin), stopSpin, FALLING);
+
+	Serial.begin(9600);
+
 	digitalWrite(dirPin, LOW);
-
-	pinMode(relayLogic1, OUTPUT);
-	pinMode(relayLogic2, OUTPUT);
-
-	//stepsPerRot = findStepsPerRot(motor1BaseSteps);
-	stepsPerRot = 400;
-	stepsToNextFly = findStepsToNextFly(stepsPerRot, numFlyTubes);
-
-	attachInterrupt(digitalPinToInterrupt(align_digital), changeSpinState, FALLING);
-
-	delay(5000);
-
-	int steps_needed = alignMotorWithIR();
-	Serial.println(steps_needed);
 }
 
 void loop() {
-	digitalWrite(LED_BUILTIN, !digitalRead(!LED_BUILTIN));
-	delay(500);
-}
-
-int findStepsToNextFly(int stepsPerRot, int numFlyTubes){
-	float circRot = 360.0;
-	int stepsToFly = floor((circRot / ((float) numFlyTubes)) * ((float) stepsPerRot));
-	return stepsToFly;
-}
-
-int findStepsPerRot(int baseSteps){
-	if (digitalRead(ms1) == HIGH)
-	{
-		baseSteps = baseSteps * 2;
+	if(switchCase == listen) {
+		Serial.println("Listening");
+		delay(2500);
+		switchCase = align;
 	}
-
-	if (digitalRead(ms2) == HIGH){
-		baseSteps = baseSteps * 4;
+	else if (switchCase == align) {
+		Serial.println("Aligning");
+		float numSteps = alignCartridge();
+		flyIndex = (flyIndex + round(numSteps / expectedSteps)) % numTubes;
+		
+		if (numSteps < 100) {
+			switchCase = align;
+		}
+		else {
+			switchCase = fireFly;
+		}
 	}
-
-	if (digitalRead(ms3) == HIGH){
-		baseSteps = baseSteps * 2;
+	else if (switchCase == fireFly) {
+		Serial.println("Firing");
+		delay(1000);
+		switchCase = verify;
 	}
-
-	return baseSteps;
-}
-
-void stepMotor(int speedVal){
-	digitalWrite(stepPin, HIGH);
-	delayMicroseconds(speedVal);
-	digitalWrite(stepPin, LOW);
-	delayMicroseconds(speedVal);
-}
-
-void chooseMotorDir(bool dir){
-	if (dir) {
-		digitalWrite(dirPin, HIGH);
+	else if (switchCase == verify) {
+		Serial.println("Verifying");
+		delay(1000);
+		switchCase = suckFly;
+	}
+	else if (switchCase == suckFly) {
+		Serial.println("Sucking");
+		delay(1000);
+		switchCase = listen;
 	}
 	else {
-		digitalWrite(dirPin, LOW);
+		Serial.println("Whoops, what happened?");
+		delay(1000);
 	}
 }
 
-void moveMotor(int steps, int speedVal, bool dir){
-	chooseMotorDir(dir);
-	for (int i = 0; i < steps; i++)
-	{
-		stepMotor(speedVal);
-	}
+void stopSpin() {
+	spinState = false;
 }
 
-void fireFly(){
-
-}
-
-int alignMotorWithIR() {
-	int steps = 0;
-	while (spin_motor == 1)
-	{
-		moveMotor(1, 600, true);
-		int val = analogRead(align_IR);
-		steps++;
-		delay(250);
-	}
+void sendStep(int val1, int val2) {
+	digitalWrite(stepPin, HIGH);
+	delayMicroseconds(val1);
 	digitalWrite(stepPin, LOW);
-	sendToMATLAB(aligned);
+	delayMicroseconds(val2);
+}
+
+float alignCartridge() {
+	digitalWrite(dirPin, LOW);
+	float steps = 0;
+	while (spinState) {
+		sendStep(10, 300);
+		steps++;
+	}
+
+	spinState = true;
 	return steps;
-}
-
-void changeSpinState() {
-	spin_motor = 0;
-}
-
-void sendToMATLAB(String code){
-	Serial.println(code);
-}
-
-String readFromMATLAB(){
-	String read = Serial.readString();
-	return read;
 }
