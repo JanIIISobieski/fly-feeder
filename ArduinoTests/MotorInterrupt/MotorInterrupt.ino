a706f6ed1035b6abf0370a5bdf4fa66f6a062649
Gabriel Antoniak <gja8@duke.edu>
2019-01-28 17:34:45 -0500

    Baseline test for reversing motor movement to get back to alignment

--- ArduinoTests/MotorInterrupt/MotorInterrupt.ino
+++ ArduinoTests/MotorInterrupt/MotorInterrupt.ino
@@ -1,8 +1,15 @@
 const int dirPin = 13;
 const int stepPin = 12;
-const int opAmpPin = 3;;
+const int opAmpPin = 3;
+
+const int rawPin = A0;
+const int potPin = A1;
 
 volatile bool spinMotorFlag = true;
+
+String part1 = "Sens1: ";
+String part2 = "   Pot1: ";
+String part3 = "   Dig: ";
 
 void setup() {
   pinMode(dirPin, OUTPUT);
@@ -14,23 +21,47 @@ void setup() {
   digitalWrite(stepPin, LOW);
 
   attachInterrupt(digitalPinToInterrupt(opAmpPin), toggleMotor, RISING);
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  //int rawInt = analogRead(rawPin);
+  //int potInt = analogRead(potPin);
+  //int digInt = digitalRead(opAmpPin);
+  //String toPrint = part1 + rawInt + part2 + potInt + part3 + digInt;
+  //Serial.println(toPrint);
   if (spinMotorFlag) {
-    sendStep(1000);
+    sendStep();
   }
   else{
+    delay(500);
+    moveMotor(200, false);
+    Serial.println("Stopping!");
     delay(5000);
     spinMotorFlag = true;
   }
 }
 
-void sendStep(int speedVal){
+void sendStep(){
   digitalWrite(stepPin, HIGH);
-  delayMicroseconds(speedVal);
+  delayMicroseconds(10);
   digitalWrite(stepPin, LOW);
-  delayMicroseconds(speedVal);
+  delayMicroseconds(300);
+}
+
+void moveMotor(int numSteps, bool dir){
+  if (!dir){
+    digitalWrite(dirPin, !digitalRead(dirPin));
+  }
+  
+  for(int i = 1; i <= numSteps; i++){
+    sendStep(); 
+  }
+  
+  if (!dir){
+    digitalWrite(dirPin, !digitalRead(dirPin));
+  }
 }
 
 void toggleMotor(){
